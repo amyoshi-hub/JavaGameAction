789a1c3f6e97b916cbbbda5aef561113100e25d5
amyoshi <123788027+amyoshi-hub@users.noreply.github.com>
2026-01-21 14:30:09 +0900

    Add files via upload

--- Player.java
+++ Player.java
@@ -10,6 +10,17 @@ public class Player extends GameObject {
 
     @Override
     public void update() {
+        super.update();
+        x += vx;
+        y += vy;
+
+        if (y < 200) {
+            vy += GRAVITY;
+        } else {
+            y = 200;
+            vy = 0;
+        }
+
         if (vx != 0) {
             anim.setState("walk");
         } else {
@@ -26,5 +37,10 @@ public class Player extends GameObject {
         // timeInt（フレーム数）を渡して、今の絵をもらう
         Image current = anim.getCurrentFrame(timeInt);
         g.drawImage(current, x, y, null);
+    }
+
+    public void setVX(float vx) { this.vx = vx; }
+    public void jump() {
+        if (y >= 200) vy = -15; // 地面にいる時だけジャンプ
     }
 }
